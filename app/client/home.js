072dc08742dacfb11dcedd71a01be2b8f9f3e1aa
Bradley Chee <bradley@airbears2-10-142-64-126.AirBears2.1918.Berkeley.EDU>
2015-10-10 18:00:10 -0700

    Implemented checking of current device's mac address

--- app/client/home.js
+++ app/client/home.js
@@ -12,7 +12,8 @@ Template.state_home.events({
 
     ble.startScan([],
         function(device) {
-            alert(JSON.stringify(device));
+            var x = device.id;
+            alert(x);
         },
         function() {
             alert("It failed!!");
@@ -21,6 +22,14 @@ Template.state_home.events({
   },
   'click #host_button': function(){
     switchAppState('createEvent');
+    MacAddress.getMacAddress(
+        function(macAddress) {
+            alert(macAddress);
+        },
+        function(fail) {
+            alert(fail);
+        }
+    );
   },
   'click #profile_button': function(){
     switchAppState('registration');
